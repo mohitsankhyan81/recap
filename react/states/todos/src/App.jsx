8d96687e3d022ff093bad92e2ed40adbbf52dce5
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-12-10 20:15:01 +0530

    commit

--- react/states/todos/src/App.jsx
+++ react/states/todos/src/App.jsx
@@ -1,27 +1,49 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const[data,setdata]= useState([]);
+
+  const [data,setdata]= useState([]);
   const [todo,settodo]=useState('');
-  
-  const handleSubmit=(e)=>{
+  const [editindex,seteditindex]=useState(null)
+
+  const handlesubmit=(e)=>{
     e.preventDefault()
-    if(todo.trim()==="") return
-    setdata([...data,todo])
+    if(todo.trim()==="")return
+
+    if(editindex==null){
+      setdata([...data,todo])
+    }
+    else{
+      data[editindex]=todo
+      setdata([...data])
+      seteditindex(null)
+    }
     settodo("")
   }
 
+  const EditTodo=(i)=>{
+    settodo(data[i])
+    seteditindex(i)
+  }
+  const DeleteTodo=(e)=>{
+    setdata(data.filter((_,i)=>i!==e))
+  }
   return (
     <div>
       <h1>Todo List</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='Enter the task' value={todo} onChange={e=>settodo(e.target.value)}/>
-        <button>Submit</button>
+      <form onSubmit={handlesubmit}>
+        <input type="text" placeholder='Enter your task' value={todo} onChange={e=>settodo(e.target.value)}/>
+
+        <button>submit</button>
       </form>
 
       <ul>
         {data.map((e,i)=>(
-          <li key={i}>{e}</li>
+          <li key={i}>
+            {e}
+            <button onClick={()=>EditTodo(i)}>Edit</button>
+            <button onClick={()=>DeleteTodo(i)}>delete</button>
+          </li>
         ))}
       </ul>
     </div>
